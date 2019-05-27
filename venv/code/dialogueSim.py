9b5a0f6bab3f44519750fecb820efcc19c8fd57c
Annelot Pruyn <>
2019-05-28 00:55:33 +0200

    all comments deleted, unused files deleted + report added

--- venv/code/dialogueSim.py
+++ venv/code/dialogueSim.py
@@ -1,11 +1,10 @@
 from languageModel import languageModel
-from BigCorpus import BigCorpus
 from corpus import corpus
 import csv
 
 class dialogueSim:
 
-    #data = pd.read_csv()
+
     def __init__(data = '../data/movie-dialog-corpus/movie_lines.tsv', self=None):
         self.data = data
 
@@ -17,21 +16,18 @@ def main():
         for row in reader:
             dialogues.append(row[-1])
 
-    #data = '../data/movie-dialog-corpus/movie_lines.tsv'
-    #corpus = BigCorpus(data)
     corp = corpus()
     corp.read_corpus(dialogues)
 
     lm = languageModel(corp)
 
-    #methods = ['greedy', 'sampling', 'beamSearch']
-    methods = ['beamSearch']
+    methods = ['greedy', 'sampling', 'beamSearch']
     for method in methods:
         print("Method: ", method)
         conversation = []
         sentence = []
         #no capital letters!
-        fourgram = ['i', 'wonder', 'how', 'the']
+        fourgram = ['how', 'much', 'do', 'we']
         for i in fourgram:
             sentence.append(i)
         print(sentence)
@@ -44,7 +40,6 @@ def main():
                 nextword = lm.endofSentence
                 sentence.append(nextword)
                 print("New senctence: ", nextword)
-                #fourgram = [fourgram[3], "</s>", "<s>", nextword]
                 fourgram = [fourgram[1], fourgram[2], fourgram[3], nextword]
 
             else:
@@ -58,11 +53,8 @@ def main():
                 print(nextword)
                 fourgram = [fourgram[1], fourgram[2],fourgram[3],nextword]
         conversation.append(sentence)
-        #print("hi there we should print the full convo now", conversation.__len__())
         speaker = ["Speaker 1:", "Speaker 2:"]
         for q, sent in enumerate(conversation):
-            #print(q)
-            #print(sent)
             speak(speaker[q%2], sent)
 
 
--- venv/code/languageModel.py
+++ venv/code/languageModel.py
@@ -51,11 +51,6 @@ class languageModel:
         print("train finished")
 
     def score(self, fourgram):
-        #print("predict started")
-        maximumCount = 0
-        maxbi = 0
-        maxuni = 0
-        bestWord = 'anecdote'
         amountFourgram = max(self.fourgramCounts[fourgram[0], fourgram[1], fourgram[2], fourgram[3]], 1)
         amountTrigram = max(self.trigramCounts[fourgram[1], fourgram[2], fourgram[3]],1)
         amountBigram = max(self.bigramCounts[fourgram[2], fourgram[3]],1)
@@ -69,12 +64,6 @@ class languageModel:
             self.scoreTwo[i] = (self.bigramCounts[fourgram[3], i] / amountUnigram)
             self.scoreOne[i] = (self.unigramCounts[i]/self.total)
             probability[i] = 0.4*self.scoreFive[i] + 0.3*self.scoreFour[i] + 0.2*self.scoreThree[i] + 0.07*self.scoreTwo[i] +0.03*self.scoreOne[i]
-        # if method == 'greedy':
-        #     return self.greedy()
-        # elif method == 'sampling':
-        #     return self.sampling()
-        # else:
-        #     return self.beamSearch(fourgram)
         return probability
 
     def greedy(self, probability):
@@ -91,8 +80,6 @@ class languageModel:
             totalValue = totalValue + probability[i]
         randomNum = random()*totalValue
         for i in probability:
-            # print("random:", randomNum)
-            #print("word: ", i, self.probability[i])
             randomNum = randomNum - probability[i];
             if randomNum <= 0:
                 bestWord = i
@@ -107,28 +94,13 @@ class languageModel:
         nodes[2] = self.extendTree(self.score([fourgram[1],fourgram[2],fourgram[3], bestWords[2]]))
         scores = [0,0,0]
 
-        #print("Best words:" , bestWords, self.probability[bestWords[0]],self.probability[bestWords[1]],self.probability[bestWords[1]])
-        #print("</s>:" , self.probability['</s>'])
-        if bestWords[0] == '</s>' and self.score([fourgram[1],fourgram[2],fourgram[3], bestWords[0]])>0.4:
+        if bestWords[0] == '</s>' and self.score([fourgram[1],fourgram[2],fourgram[3], bestWords[0]])>0.6:
             return bestWords[0]
-        # #evaluate first word
-        # phrase1[0] = self.score([fourgram[1], fourgram[2], fourgram[3], bestWords[0]], 'sampling')
-        # phrase1[1] = self.score([fourgram[2], fourgram[3], bestWords[0], phrase1[0]], 'sampling')
-        #
-        # # evaluate second word
-        # phrase2[0] = self.score([fourgram[1], fourgram[2], fourgram[3], bestWords[1]], 'sampling')
-        # phrase2[1] = self.score([fourgram[2], fourgram[3], bestWords[1], phrase2[0]], 'sampling')
-        #
-        # # evaluate first word
-        # phrase3[0] = self.score( [fourgram[1], fourgram[2], fourgram[3], bestWords[2]], 'sampling')
-        # phrase3[1] = self.score( [fourgram[2], fourgram[3], bestWords[2], phrase3[0]], 'sampling')
 
-            for i,j in range(0,2,1):
-                scores[i] = scores[i] + 3*self.fivegramCounts[fourgram[1], fourgram[2], fourgram[3], bestWords[i], nodes[i][j]] + 2*self.fourgramCounts[fourgram[2], fourgram[3], bestWords[i], nodes[i][j]] + self.trigramCounts[ fourgram[3], bestWords[i], nodes[i][j]]
-
-        # scores[0] = 3*self.fivegramCounts[fourgram[1], fourgram[2], fourgram[3], bestWords[0], nodes1[0]] + 3*self.fivegramCounts[fourgram[1], fourgram[2], fourgram[3], bestWords[0], nodes1[]] + 2*self.fourgramCounts[fourgram[2], fourgram[3], bestWords[0], phrase1[0]] + 2*self.fourgramCounts[fourgram[3], bestWords[0], phrase1[0], phrase1[1]] + self.trigramCounts[ fourgram[3], bestWords[0], phrase1[0]] + self.trigramCounts[bestWords[0], phrase1[0], phrase1[1]]
-        # scores[1] = 3*self.fivegramCounts[fourgram[1], fourgram[2], fourgram[3], bestWords[1], phrase2[0]] + 3*self.fivegramCounts[fourgram[2], fourgram[3], bestWords[1], phrase2[0], phrase2[1]] + 2*self.fourgramCounts[fourgram[2], fourgram[3], bestWords[1], phrase2[0]] + 2*self.fourgramCounts[fourgram[3], bestWords[1], phrase2[0], phrase2[1]] + self.trigramCounts[ fourgram[3], bestWords[1], phrase2[0]] + self.trigramCounts[bestWords[1], phrase2[0], phrase2[1]]
-        # scores[2] = 3*self.fivegramCounts[fourgram[1], fourgram[2], fourgram[3], bestWords[2], phrase3[0]] + 3*self.fivegramCounts[fourgram[2], fourgram[3], bestWords[2], phrase3[0], phrase3[1]] + 2*self.fourgramCounts[fourgram[2], fourgram[3], bestWords[2], phrase3[0]] + 2*self.fourgramCounts[fourgram[3], bestWords[2], phrase3[0], phrase3[1]] + self.trigramCounts[ fourgram[3], bestWords[2], phrase3[0]] + self.trigramCounts[bestWords[2], phrase3[0], phrase3[1]]
+        for k in range(3):
+            for l in range(3):
+                scores[k] = scores[k] + 3*self.fivegramCounts[fourgram[1], fourgram[2], fourgram[3], bestWords[k], nodes[k][l]] + 2*self.fourgramCounts[fourgram[2], fourgram[3], bestWords[k], nodes[k][l]] + self.trigramCounts[fourgram[3], bestWords[k], nodes[k][l]]
+                #print("Score: ", k, scores[k])
 
         maximum = max(scores[0],scores[1],scores[2])
 
@@ -142,8 +114,6 @@ class languageModel:
         bestWords = ['anecdote', 'anecdote', 'anecdote']
         for i in probability:
             if probability[i] > probability[bestWords[2]]:
-                # if i == '</s>':
-                # print("its better than the others we can celebrate", i)
                 if probability[i] > probability[bestWords[1]]:
                     if probability[i] > probability[bestWords[0]]:
                         bestWords[2] = bestWords[1]
@@ -158,7 +128,6 @@ class languageModel:
 
     @property
     def endofSentence(self):
-        print("started endofSentence")
         maximumCount = 0
         bestWord = 'anecdote'
         totalSentences = self.unigramCounts["<s>"]
@@ -171,14 +140,6 @@ class languageModel:
                 bestWord = i
                 break
 
-
-        #for i in self.unigramCounts.keys():
-        #    if self.bigramCounts["<s>", i] > maximumCount:
-        #        maximumCount = self.bigramCounts["<s>", i]
-         #       bestWord = i
-                #print(maximumCount)
-                #print("best word", i)
-        print("finished endofSentence")
         return bestWord
 
     def printTrigram(self):
